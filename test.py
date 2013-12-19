351e89744c3e0950ec6ead97305f47db5aaf9475
Jingyue Wu <wujingyue@gmail.com>
2013-12-19 16:20:22 -0500

    test amazon s3

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
-
-import argparse
+import httplib
+from contextlib import closing
 
 if __name__ == '__main__':
-    return 0
+    with closing(httplib.HTTPSConnection('nimbledroid.s3.amazonaws.com')) as conn:
+        with open('test.c', 'r') as f:
+            conn.request('PUT', '/test.c', f, {'x-amz-grant-read': 'emailAddress="junfeng@cs.columbia.edu"'})
+            r = conn.getresponse()
+            print r.status
